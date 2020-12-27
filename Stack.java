4e0c54d7b9c29d00610bc52b331256050da665ee
rohan7795 <rbattulwar99@gmail.com>
2020-12-27 23:24:01 +0530

    Final Stack DS

---
+++ Stack.java
@@ -0,0 +1,13 @@
+package com.stackexample.com;
+
+public interface Stack {
+	public int size();
+	public boolean isEmpty();	
+	public Object top();
+
+	public Object push(Object elemennt);
+	public Object pop();
+	public Object printAll();
+}
+
+
